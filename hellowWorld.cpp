e7f17456f8060e893679a78daa31f89d39ddf358
Damon <cman55245@gmail.com>
2025-02-02 15:25:25 +0530

    Initial Commit

---
+++ hellowWorld.cpp
@@ -0,0 +1,11 @@
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+int main(){
+     
+    // This statement prints "Hello World"
+    cout << "Hello World";
+
+    return 0;
+}
